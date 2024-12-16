a6be5098800ca4b0401be28d21fcab4a6ba6edfc
carla-lm <113684954+carla-lm@users.noreply.github.com>
2024-12-16 11:22:59 +0100

    Completed code

--- 9_gym-DDPG/DDPG.py
+++ 9_gym-DDPG/DDPG.py
@@ -21,10 +21,15 @@ class UnsupportedSpace(Exception):
         super().__init__(self.message)
 
 class QFunction(Feedforward):
-    def __init__(self, observation_dim, action_dim, hidden_sizes=[100,100],
-                 learning_rate = 0.0002):
+    def __init__(self, observation_dim, action_dim, hidden_sizes=[100,100], learning_rate = 0.0002):
         # TODO: Setup network with right input and output size (using super().__init__)
-
+        ##########
+        input_size = observation_dim + action_dim
+        output_size = 1  # Result is a scalar value
+        super().__init__(input_size=input_size, hidden_sizes=hidden_sizes, output_size=output_size)
+        self.observation_dim = observation_dim
+        self.action_dim = action_dim
+        ##########
         # END
         self.optimizer=torch.optim.Adam(self.parameters(),
                                         lr=learning_rate,
@@ -47,7 +52,14 @@ class QFunction(Feedforward):
 
     def Q_value(self, observations, actions):
         # TODO: implement the forward pass.
-        pass
+        ##########
+        # Concatenate observations and actions along the last dimension
+        combined_input = torch.cat([observations, actions], dim=-1)
+
+        # Use Feedforward's forward method to get Q-value
+        q_value = self(combined_input)  # This calls Feedforward's forward method
+        return q_value
+        ##########
 
 # Ornstein Uhlbeck noise, Nothing to be done here
 class OUNoise():
@@ -84,7 +96,7 @@ class DDPGAgent(object):
                                    ' (Require Box)'.format(action_space, self))
 
         self._observation_space = observation_space
-        self._obs_dim=self._observation_space.shape[0]
+        self._obs_dim = self._observation_space.shape[0]
         self._action_space = action_space
         self._action_n = action_space.shape[0]
         self._config = {
@@ -109,13 +121,13 @@ class DDPGAgent(object):
         # Q Network
         self.Q = QFunction(observation_dim=self._obs_dim,
                            action_dim=self._action_n,
-                           hidden_sizes= self._config["hidden_sizes_critic"],
-                           learning_rate = self._config["learning_rate_critic"])
+                           hidden_sizes=self._config["hidden_sizes_critic"],
+                           learning_rate=self._config["learning_rate_critic"])
         # target Q Network
         self.Q_target = QFunction(observation_dim=self._obs_dim,
                                   action_dim=self._action_n,
-                                  hidden_sizes= self._config["hidden_sizes_critic"],
-                                  learning_rate = 0)
+                                  hidden_sizes=self._config["hidden_sizes_critic"],
+                                  learning_rate=0)
 
         high, low = torch.from_numpy(self._action_space.high), torch.from_numpy(self._action_space.low)
         # TODO:
@@ -123,7 +135,9 @@ class DDPGAgent(object):
         # and makes sure the derivative goes to zero at the boundaries
         # Use Tanh, which is between -1 and 1 and scale it to [low, high]
         # Hint: use torch.nn.Tanh()(x)
-        output_activation = lambda x: pass
+        ##########
+        output_activation = lambda x: (torch.nn.Tanh()(x) * (high - low) / 2) + (high + low) / 2
+        ##########
 
         self.policy = Feedforward(input_size=self._obs_dim,
                                   hidden_sizes= self._config["hidden_sizes_actor"],
@@ -149,7 +163,16 @@ class DDPGAgent(object):
 
     def act(self, observation, eps=None):
         # TODO: implement this: use self.action_noise() (which provides normal noise with standard variance)
-
+        ##########
+        """Compute action using policy and add noise for exploration."""
+        if eps is None:
+            eps = self._eps
+        obs_tensor = torch.from_numpy(observation.astype(np.float32))
+        with torch.no_grad():
+            action = self.policy(obs_tensor).numpy()
+        noise = self.action_noise() * eps
+        return np.clip(action + noise, self._action_space.low, self._action_space.high)
+        ##########
 
     def store_transition(self, transition):
         self.buffer.add_transition(transition)
@@ -183,7 +206,28 @@ class DDPGAgent(object):
             # TODO: Implement the rest of the algorithm
 
             # assign q_loss_value  and actor_loss to we stored in the statistics
+            ##########
+            # Critic loss (TD error)
+            with torch.no_grad():
+                next_action = self.policy_target(s_prime)
+                q_target_next = self.Q_target(torch.cat((s_prime, next_action), dim=1))
+                y = rew + self._config["discount"] * (1 - done) * q_target_next
 
+            q_value = self.Q.Q_value(s, a)
+            q_loss_value = torch.nn.functional.mse_loss(q_value, y)
+
+            self.Q.optimizer.zero_grad()
+            q_loss_value.backward()
+            self.Q.optimizer.step()
+
+            # Actor loss
+            pred_action = self.policy(s)
+            actor_loss = -self.Q.Q_value(s, pred_action).mean()
+
+            self.optimizer.zero_grad()
+            actor_loss.backward()
+            self.optimizer.step()
+            ##########
 
             losses.append((q_loss_value , actor_loss.item()))
 
@@ -193,7 +237,7 @@ class DDPGAgent(object):
 def main():
     optParser = optparse.OptionParser()
     optParser.add_option('-e', '--env',action='store', type='string',
-                         dest='env_name',default="Pendulum-v1",
+                         dest='env_name',default="HalfCheetah-v4",
                          help='Environment (default %default)')
     optParser.add_option('-n', '--eps',action='store',  type='float',
                          dest='eps',default=0.1,
@@ -205,13 +249,13 @@ def main():
                          dest='lr',default=0.0001,
                          help='learning rate for actor/policy (default %default)')
     optParser.add_option('-m', '--maxepisodes',action='store',  type='float',
-                         dest='max_episodes',default=2000,
+                         dest='max_episodes',default=6000,
                          help='number of episodes (default %default)')
     optParser.add_option('-u', '--update',action='store',  type='float',
                          dest='update_every',default=100,
                          help='number of episodes between target network updates (default %default)')
     optParser.add_option('-s', '--seed',action='store',  type='int',
-                         dest='seed',default=None,
+                         dest='seed',default=1,
                          help='random seed (default %default)')
     opts, args = optParser.parse_args()
     ############## Hyperparameters ##############
@@ -287,3 +331,98 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+
+# def main(lr, update_every, random_seed):
+#     optParser = optparse.OptionParser()
+#     optParser.add_option('-e', '--env', action='store', type='string',
+#                          dest='env_name', default="Pendulum-v1",
+#                          help='Environment (default %default)')
+#     optParser.add_option('-n', '--eps', action='store', type='float',
+#                          dest='eps', default=0.1,
+#                          help='Policy noise (default %default)')
+#     optParser.add_option('-t', '--train', action='store', type='int',
+#                          dest='train', default=32,
+#                          help='number of training batches per episode (default %default)')
+#     optParser.add_option('-l', '--lr', action='store', type='float',
+#                          dest='lr', default=lr,
+#                          help='learning rate for actor/policy (default %default)')
+#     optParser.add_option('-m', '--maxepisodes', action='store', type='float',
+#                          dest='max_episodes', default=2000,
+#                          help='number of episodes (default %default)')
+#     optParser.add_option('-u', '--update', action='store', type='float',
+#                          dest='update_every', default=update_every,
+#                          help='number of episodes between target network updates (default %default)')
+#     optParser.add_option('-s', '--seed', action='store', type='int',
+#                          dest='seed', default=random_seed,
+#                          help='random seed (default %default)')
+#     opts, args = optParser.parse_args()
+#
+#     # Create the environment
+#     env_name = opts.env_name
+#     env = gym.make(env_name)
+#
+#     # Set the seed for random number generators
+#     if random_seed is not None:
+#         torch.manual_seed(random_seed)
+#         np.random.seed(random_seed)
+#
+#     ddpg = DDPGAgent(env.observation_space, env.action_space, eps=opts.eps, learning_rate_actor=opts.lr,
+#                      update_target_every=opts.update_every)
+#
+#     rewards = []
+#     lengths = []
+#     losses = []
+#     timestep = 0
+#
+#     def save_statistics():
+#         file_name = f"./results/DDPG_{env_name}_eps{opts.eps}_t{opts.train}_l{opts.lr}_update{opts.update_every}_s{random_seed}_stat.pkl"
+#         with open(file_name, 'wb') as f:
+#             pickle.dump({"rewards": rewards, "lengths": lengths, "eps": opts.eps, "train": opts.train,
+#                          "lr": opts.lr, "update_every": opts.update_every, "losses": losses}, f)
+#
+#     # Training loop
+#     for i_episode in range(1, opts.max_episodes + 1):
+#         ob, _info = env.reset()
+#         ddpg.reset()
+#         total_reward = 0
+#         for t in range(2000):  # max_timesteps
+#             timestep += 1
+#             done = False
+#             a = ddpg.act(ob)
+#             ob_new, reward, done, trunc, _info = env.step(a)
+#             total_reward += reward
+#             ddpg.store_transition((ob, a, reward, ob_new, done))
+#             ob = ob_new
+#             if done or trunc: break
+#
+#         losses.extend(ddpg.train(opts.train))
+#
+#         rewards.append(total_reward)
+#         lengths.append(t)
+#
+#         # Save checkpoint every 500 episodes
+#         if i_episode % 500 == 0:
+#             checkpoint_filename = f'./results/DDPG_{env_name}_eps{opts.eps}_t{opts.train}_l{opts.lr}_update{opts.update_every}_s{random_seed}_{i_episode}.pth'
+#             torch.save(ddpg.state(), checkpoint_filename)
+#             save_statistics()
+#
+#         # Logging
+#         if i_episode % 20 == 0:  # log_interval
+#             avg_reward = np.mean(rewards[-20:])
+#             avg_length = int(np.mean(lengths[-20:]))
+#             print(f'Episode {i_episode} \t avg length: {avg_length} \t reward: {avg_reward}')
+#
+#     save_statistics()
+
+
+# if __name__ == '__main__':
+#     learning_rates = [0.001, 0.0005, 0.0001, 0.00005]
+#     update_frequencies = [20, 100]
+#
+#     for lr in learning_rates:
+#         for update in update_frequencies:
+#             # Generate a unique seed based on the combination of learning rate and update frequency
+#             seed = hash((lr,update)) % 1000  # Limit the seed to a 3-digit integer (0-999)
+#             print(f"Running experiment with lr={lr}, update_every={update}, seed={seed}")
+#             main(lr=lr, update_every=update, random_seed=seed)
